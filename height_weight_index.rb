bfd723edcea0fe5e281221c6830bfe2ec5c6077c
Batuhan Aydin <114925623+batuhannaydin@users.noreply.github.com>
2023-10-08 12:11:53 +0300

    Update height_weight_index.rb

--- height_weight_index.rb
+++ height_weight_index.rb
@@ -1,43 +1,19 @@
 # frozen_string_literal: true
 
+def ideal_weight(height, gender) 
+    if gender == "M"
+        (height - 100) - (height - 150) / 4
+    elsif gender == "W"
+        (height - 100) - (height - 150) / 2
+    end
+end
+
 height, weight, gender = gets.chomp.split
 height = height.to_i
 weight = weight.to_i
+ideal_weight = ideal_weight(height, gender)
 
-puts "enter your height(cm), weighth(kg) and gender (W, M)"
-
-def ideal_weight(height,gender)
-    if gender == "W"
-        def ideal_weight_women(height)
-            ideal_weight_women = (height - 100) - (height - 150)/2
-        end
-    elsif gender == "M"
-        def ideal_weight_man(height)
-            ideal_weight_man = (height - 100) - (height - 150)/4
-        end
-    end
+if ideal_weight > weight then puts "skinny"
+elsif ideal_weight < weight then puts "fat"
+elsif ideal_weight = weight then puts "normal"
 end
-
-if ideal_weight(height, gender)
-    if gender == "M"
-        if weight < ideal_weight_man(height)
-            puts "Your weight is below normal"
-        elsif weight > ideal_weightM(height)
-            puts "fat"
-        else
-            puts "normal weight"
-        end
-    elsif gender == "W"
-        if weight < ideal_weight_women(height)
-            puts "Your weight is below normal"
-        elsif weight > ideal_weightW(height)
-            puts "fat"
-        else
-            puts "normal weight"
-        end
-    else
-         puts "wrong data entry"
-    end
-end
-
-
